1f7c2aff792bc7471496c784606b91553a5cdb10
danielecursano <dacu1405@gmail.com>
2026-03-06 20:56:57 +0100

    add example of sofie and hls4ml integration

--- gsoc/main.py
+++ gsoc/main.py
@@ -1,9 +1,7 @@
-from exercise_4 import get_model_config
-from exercise_5 import generate_sofie_model
-
 import hls4ml
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 
 model = keras.Sequential([
     keras.layers.InputLayer(input_shape=(10,)),
@@ -12,12 +10,15 @@ model = keras.Sequential([
     keras.layers.Activation("elu")
 ])
 
-config = hls4ml.utils.config_from_keras_model(model)
-hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=config)
+config = hls4ml.utils.config_from_keras_model(model, backend='sofie')
+hls_model = hls4ml.converters.convert_from_keras_model(model, hls_config=config, backend='sofie')
 
-model_config = get_model_config(hls_model)
+hls_model.write()
+x = np.random.rand(10).astype(np.float32)
+y = hls_model.predict(x)
+print(y)
+print(model.predict(x.reshape((1, 10))))
 
-rmodel = generate_sofie_model(model_config)
-rmodel.Generate()
-rmodel.OutputGenerated()
-rmodel.PrintGenerated()
+# Alternative method to create the session once and reuse it
+sofie_session = hls4ml.backends.SofieBackend.get_sofie_session(hls_model)
+print(sofie_session.infer(x))
