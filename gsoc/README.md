75dd1e7af7d387a5603f1b5ebf972e30ba67eb6b
danielecursano <dacu1405@gmail.com>
2026-03-09 11:59:42 +0100

    update sofie backend parent class

--- gsoc/README.md
+++ gsoc/README.md
@@ -7,7 +7,7 @@ I also wrote some tests in the `test` folder that you can run using `pytest`.
 
 In `main.py`, you can find a possible integration flow between SOFIE and `hls4ml`. The idea is to create a custom backend in `hls4ml` for SOFIE.
 
-In the `sofie_backend` directory, you can find the source code of the new backend, which inherits from `FPGABackend`. In `main.py`, I register the backend and its writer.
+In the `sofie_backend` directory, you can find the source code of the new backend. In `main.py`, I register the backend and its writer.
 
 The backend follows the default flow used by other HLS backends. In Python, given a model from a popular framework, you can parse the model and pass it to the SOFIE backend. You can then compile the model, which will:
 
@@ -19,3 +19,6 @@ Alternatively, you can extract the SOFIE session directly using the following st
 
 ```python
 SofieBackend.get_sofie_session(hls_model)
+```
+
+This way, SOFIE could reuse the existing hls4ml tests and the optimization flows already implemented.
--- gsoc/sofie_backend/sofie_backend.py
+++ gsoc/sofie_backend/sofie_backend.py
@@ -1,16 +1,20 @@
 import ROOT
 import numpy as np
+import subprocess
 
-from hls4ml.backends import FPGABackend
+from hls4ml.backends import Backend
 from hls4ml.model.flow import register_flow
+from hls4ml.model.types import StandardFloatPrecisionType
+from hls4ml.model.optimizer import model_optimizer
+from hls4ml.writer import get_writer
 
-class SofieBackend(FPGABackend):
+class SofieBackend(Backend):
     def __init__(self):
         super().__init__('Sofie')
         initializers = self._get_layer_initializers()
         self._default_flow = register_flow('init_layers', initializers, requires=[], backend=self.name)
         self._writer_flow = register_flow('write', ['make_stamp', 'sofie:write_hls'], requires=[self._default_flow], backend=self.name)
-        # TODO add flows for optimization?
+        self.writer = get_writer(self.name)
         
     def create_initial_config(self, **kwargs):
         return dict()
@@ -20,6 +24,38 @@ class SofieBackend(FPGABackend):
             
     def get_writer_flow(self):
         return self._writer_flow
+        
+    def create_layer_class(self, layer_class):
+        # No additional attributes for layer classes
+        return layer_class
+        
+    def write(self, model):
+        model.apply_flow(self.get_writer_flow())
+        
+    def compile(self, model):
+        # Inherited from FPGABackend. ModelGraph still needs a .so to extract the top function
+        lib_name = None
+        ret_val = subprocess.run(
+            ['./build_lib.sh'],
+            shell=True,
+            text=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            cwd=model.config.get_output_dir(),
+        )
+        if ret_val.returncode != 0:
+            print(ret_val.stdout)
+            raise Exception(f'Failed to compile project "{model.config.get_project_name()}"')
+        lib_name = '{}/firmware/{}-{}.so'.format(
+            model.config.get_output_dir(), model.config.get_project_name(), model.config.get_config_value('Stamp')
+        )
+
+        return lib_name
+    
+    @classmethod    
+    def convert_precision_string(cls, precision):
+        # SOFIE RModel supports only float precision at the moment
+        return StandardFloatPrecisionType(width=32, exponent=8, use_cpp_type=True)
     
     @staticmethod
     def get_sofie_session(model):
@@ -30,6 +66,11 @@ class SofieBackend(FPGABackend):
             raise RuntimeError(f"SOFIE namespace TMVA_SOFIE_{model.config.get_project_name()} not found.")
         session = sofie_project.Session(header_path+".dat")
         return session
+        
+    @model_optimizer()
+    def write_hls(self, model):
+        self.writer.write_hls(model)
+        return True
         
         
         
